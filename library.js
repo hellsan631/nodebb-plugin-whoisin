bcf46d455744b712553bea420206f163375c9a81
Peter Jaszkowiak <p.jaszkow@gmail.com>
2014-11-25 22:38:55 -0700

    Include whoisin with other projects

--- library.js
+++ library.js
@@ -6,18 +6,27 @@
 var whoisin = {},
 		SocketPlugins = module.parent.require('./socket.io/plugins'),
 		db = module.parent.require('./database'),
-		// meta = module.parent.require('./meta'),
+	  meta = module.parent.require('./meta'),
 		user = module.parent.require('./user'),
 		// posts = module.parent.require('./posts'),
 		mainTemplate =
 		// TODO: get from'/templates/views/main';
-		'<div class="whoisin row">' +
+		'<div class="whoisin">' +
 		'  <h4>Who is in?</h4>' +
-		'  <button class="iamin btn btn-primary">I am in!</button>' +
-		'</div>';
+		'	<div class="participants"></div>' +
+		'  <div class="whoisin-btn-wrapper">' +
+		' 	<button title="add yourself to the list" class="iamin btn btn-primary">' +
+		' 		<i class="fa fa-plus"></i> I am in!</button>' +
+		' 	<button title="remove yourself from this list" class="iamnotin btn btn-danger">' +
+		' 		<i class="fa fa-times"></i></button>' +
+		'	</div>' +
+		'</div>' +
+		'<br />',
+          idInput = '<input type="hidden" class="whoisin-id" style="display:none;" value="{{id}}" />';
 
 whoisin.init = function(app, middleware, controllers, callback) {
 	console.log('nodebb-plugin-whoisin: loaded');
+	console.log('configs ------------------> ', meta.config);
 
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
@@ -49,20 +58,27 @@ whoisin.parse = function(postContent, callback) {
 
 whoisin.commit = function(socket, data, callback) {
 	if (socket.hasOwnProperty('uid') && socket.uid > 0) {
-		var topicid = data.url.match("topic/([0-9]*)")[1];
-		db.getObject('post-' + topicid + '-whoisin_participants', function(err, whoisin_participants) {
+		var topicid = data.id || data.url.match("topic/([0-9]*)")[1];
+		db.getObject('whoisin-post-' + topicid + '-participants', function(err, whoisin_participants) {
 			if (err) {
 				console.log('whoisin plugin: noone is in');
 			}
-			whoisin_participants[socket.uid] = {
-				isin: true,
-				timestamp: new Date()
-			};
-			db.setObject('post-' + topicid + '-whoisin_participants', whoisin_participants , function(err){
+			whoisin_participants = whoisin_participants || {};
+			if (data.action === 'add') {
+				whoisin_participants[socket.uid] = {
+					isin: true,
+					timestamp: new Date()
+				};
+			} else if (data.action === 'remove' && whoisin_participants[socket.uid]) {
+				whoisin_participants[socket.uid].isin = false;
+			}
+			//serialize
+			whoisin_participants[socket.uid] = JSON.stringify(whoisin_participants[socket.uid]);
+
+			db.setObject('whoisin-post-' + topicid + '-participants', whoisin_participants, function(err){
 				if (err) {
 					console.log('Whoisin Plugin: Error saving to db, ', err);
 				} else {
-					console.log('saved to db: ', JSON.stringify(whoisin_participants));
 					callback(null, "success");
 				}
 			});
@@ -73,19 +89,37 @@ whoisin.commit = function(socket, data, callback) {
 }
 
 whoisin.load = function(socket, data, callback) {
-	var topicid = data.url.match("topic/([0-9]*)")[1];
-	db.getObject('post-' + topicid + '-whoisin_participants', function(err, data) {
+	var topicid = data.id || data.url.match("topic/([0-9]*)")[1];
+
+	db.getObject('whoisin-post-' + topicid + '-participants', function(err, participants) {
+		var users_array = [];
+		var whoisin_data = {};
 		if (err) {
 			console.log('whoisin plugin: Error getting list of participants for topic');
 		}
-		console.log('got participant object: ', JSON.stringify(data));
-		var users_array = [];
-		for (var userid in data) {
+		whoisin_data.current_user_is_in = false;
+		whoisin_data.current_user_id = socket.uid;
+		whoisin_data.users = participants;
+		for (var userid in whoisin_data.users) {
+			// FIXME
+			whoisin_data.users[userid] = JSON.parse(whoisin_data.users[userid]);
+
 			users_array.push(userid);
+			if (userid == socket.uid && whoisin_data.users[userid].isin) {
+				whoisin_data.current_user_is_in = true;
+			}
 		}
-		console.log('got user array :-> ', users_array);
-		user.getMultipleUserFields(users_array, ['username', 'userslug', 'picture'], function(err, userData) {
-			callback(null, userData);
+		user.getMultipleUserFields(users_array, ['username', 'userslug', 'picture', 'uid'], function(err, users_data) {
+			// Add user data to whoisin data
+			for (var id in users_data) {
+				var user = users_data[id];
+				whoisin_data.users[user.uid].userslug = user.userslug;
+				whoisin_data.users[user.uid].picture = user.picture;
+				// for (var prop in user) {
+				// 	whoisin_data.users[user.uid][prop] = user[prop];
+				// }
+			}
+			callback(null, whoisin_data);
 		});
 	});
 }
@@ -94,6 +128,15 @@ function renderAdmin(req, res, next) {
 	res.render('admin/plugins/whoisin', {});
 }
 
+whoisin.include = function(id, callback){
+    
+  var html = mainTemplate + idInput.replace("{{id}}", id);
+
+  if(callback) { callback(null, html); }
+
+  return html;
+}
+  
 module.exports = whoisin;
 
 }(module));
--- static/lib/main.js
+++ static/lib/main.js
@@ -2,39 +2,94 @@
 	"use strict";
 
 	window.WhoisinPlugin = {
+        id: "",
 		load: function(data) {
 			socket.emit('plugins.whoisin.load', {
-				url: data.url
-			}, function(err, users) {
-				console.log('loading who is in plugin with ', users);
-				for (var user in users) {
-					$('div.whoisin').append('<div class="whoisin-user"> User: ' + user + '</div>')
+				url: data.url,
+                id: WhoisinPlugin.id
+			}, function(err, whoisin_data) {
+				var participantsDiv = $('div.participants');
+				participantsDiv.html('');
+				for (var id in whoisin_data.users) {
+					var user = whoisin_data.users[id]
+					if (user.isin) {
+						participantsDiv.append(
+							'<a href="/user/' + user.username + '">' +
+							'<img data-original-title="' + user.userslug + '" src="' +
+							user.picture + '" alt="' + user.slug + '" class="whoisin-avatar"' +
+							' title=""></a>');
+					}
+				}
+				if (!!whoisin_data.current_user_id) {
+					if (whoisin_data.current_user_is_in) {
+						$('div.whoisin-btn-wrapper button.iamnotin').removeClass('hidden')
+						$('div.whoisin-btn-wrapper button.iamin').addClass('hidden')
+					} else {
+						$('div.whoisin-btn-wrapper button.iamnotin').addClass('hidden')
+						$('div.whoisin-btn-wrapper button.iamin').removeClass('hidden')
+					}
+				} else {
+					$('div.whoisin-btn-wrapper button.iamnotin').addClass('hidden')
+					$('div.whoisin-btn-wrapper button.iamin').addClass('hidden')
 				}
 			});
-		}
-	}
+		},
+        setup: function(ev, data) {
+            data = data || {};
+            data.id = WhoisinPlugin.id = $(".whoisin-id").val();
+            if (data.id || /^topic\/[\d]+/.test(data.url)) {
+                // load existing users into whoisin widget
+                WhoisinPlugin.load(data);
+                // add currently logged in user when iamin button is tapped
+                $('div.whoisin-btn-wrapper button.iamin').on('click', function(e) {
+                    socket.emit('plugins.whoisin.commit', {
+                        url: data.url,
+                        id: data.id,
+                        action: 'add'
+                    }, function(err, result) {
+                        if (err) {
+                            // TODO: handle error
+                        } else {
+                            WhoisinPlugin.load(data);
+                        }
+                    });
+                });
 
-	$(window).on('action:ajaxify.end', function(ev, data) {
-		if (/^topic\/[\d]+/.test(data.url)) {
-			// load existing users into whoisin widget
-			WhoisinPlugin.load(data);
+                // remove the current user from the whois in list
+                $('div.whoisin-btn-wrapper button.iamnotin').on('click', function(e) {
+                    bootbox.dialog({
+                    message: "You are about to remove yourself from the list",
+                    title: "You are no longer in?",
+                        buttons: {
+                            danger: {
+                                label: "Remove me!",
+                                className: "btn-danger",
+                                callback: function() {
+                                    socket.emit('plugins.whoisin.commit', {
+                                        url: data.url,
+                                        id: data.id,
+                                        action: 'remove'
+                                    }, function(err, result) {
+                                        if (err) {
+                                            // TODO: handle error
+                                        } else {
+                                            WhoisinPlugin.load(data);
+                                        }
+                                    });
+                                }
+                            },
+                            main: {
+                                label: "Cancel",
+                                className: "btn-primary",
+                                callback: function() {}
+                            }
+                        }
+                    });
 
-			// add currently logged in user when iamin button is tapped
-			$('div.whoisin button.iamin').on('click', function(e) {
-				socket.emit('plugins.whoisin.commit', {
-					url: data.url
-				}, function(err, result) {
-					if (err) {
-						// TODO: handle error
-					} else {
-						console.log('getting users result: ' + result);
-						WhoisinPlugin.load(data);
-					}
-				});
-			});
-		}
-	});
+                });
+            }
+       }
+	};
 
-
-	console.log('nodebb-plugin-whoisin: is loaded');
+	$(window).on('action:ajaxify.end', WhoisinPlugin.setup);
 }());
